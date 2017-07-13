5c15cccf15c16d5bd1e0db560e39d6c12bb28dfc
ryan-martin-lm <ryan.martin01@libertymutual.com>
2017-07-13 14:57:36 -0400

    Removed authentication

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var auth = require('http-auth');
+/*
 var basic = auth.basic({
     realm: "broccoli-project", // change this to your client / project name
     file: __dirname + "/password" // gevorg:gpass, Sarah:testpass ...
 });
+*/
 var express = require('express');
 var app = express();
 
-app.use(auth.connect(basic));
+//app.use(auth.connect(basic));
 app.use(express.static('dist'));
 var server = app.listen(process.env.PORT || 8088, function () {
   var host = server.address().address;
